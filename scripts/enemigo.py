88b428edab5d339647cacbcdbe8a9181f78a2da7
VlackoDalze <bladimil124@gmail.com>
2023-04-15 23:59:58 +0200

    Update enemigo.py

--- scripts/enemigo.py
+++ scripts/enemigo.py
@@ -1,4 +1,3 @@
-
 import pygame
 from scripts.statistics import Statistics
 import scripts.setting as setting
@@ -10,26 +9,40 @@ SCREEN_WIDTH = setting.SCREEN_WIDTH
 SCREEN_HEIGHT = setting.SCREEN_HEIGHT
 letter_style = "assets/font/Silver.ttf"
 
-font='assets/dungeon/floor/sandstone_floor_0.png'
-color=(255, 255, 255)    
-texture_enemy=[["Esqueleto1","assets/monster/undead/skeletons/skeleton_humanoid_small_old.png"],
-               ["Esqueleto2","assets/monster/undead/skeletons/skeleton_humanoid_small_new.png"]]
-collision_enemy=[]
+font = "assets/dungeon/floor/sandstone_floor_0.png"
+color = (255, 255, 255)
+texture_enemy = [
+    ["Esqueleto1", "assets/monster/undead/skeletons/skeleton_humanoid_small_old.png"],
+    ["Esqueleto2", "assets/monster/undead/skeletons/skeleton_humanoid_small_new.png"],
+]
+collision_enemy = []
 
-class Enemy():
-    
-    index_animation=0
-    iteration=0
-    def searchEnemy(nombre:str):
-        animation_enemy=[]
+
+class Enemy:
+    index_animation = 0
+    iteration = 0
+
+    def searchEnemy(nombre: str):
+        animation_enemy = []
         for enemy in texture_enemy:
             if str(enemy[0]).startswith(nombre):
                 animation_enemy.append(enemy[1])
         return animation_enemy
-    
-    #constructor
-    def __init__(self, screen: pygame.Surface, nombre: str, descripcion: str,nivel:int, estadisticasBase: Statistics, habilidadesActivas, habilidadesPasivas, posicionX:int, posicionY:int,scene_level):
 
+    # constructor
+    def __init__(
+        self,
+        screen: pygame.Surface,
+        nombre: str,
+        descripcion: str,
+        nivel: int,
+        estadisticasBase: Statistics,
+        habilidadesActivas,
+        habilidadesPasivas,
+        posicionX: int,
+        posicionY: int,
+        scene_level,
+    ):
         self.screen = screen
         self.nombre = nombre
         self.descripcion = descripcion
@@ -38,44 +51,35 @@ class Enemy():
         self.habilidadesActivas = habilidadesActivas
         self.habilidadesPasivas = habilidadesPasivas
         self.posicionX = posicionX
-        self.habilidadesActivas=habilidadesActivas
+        self.habilidadesActivas = habilidadesActivas
         self.posicionY = posicionY
         self.scene_level = scene_level
-        self.nivel=nivel
-
+        self.nivel = nivel
 
     def getPositionX(self):
         return self.posicionX
 
-    def getPositionY(self ):
-        return self.posicionY   
-    
+    def getPositionY(self):
+        return self.posicionY
+
     def drawEnemy(self):
-        
-        enemy=pygame.Surface((CELL_SIZE,CELL_SIZE))
-        fondo=pygame.image.load(font)
-        image=pygame.image.load(self.imagen[Enemy.index_animation])
-        #mix2=pygame.image.load(self.imagen[1])
-        enemy.blit(fondo, (0,0))
-        enemy.blit(image, (0,0))
-        self.screen.blit(enemy,(self.posicionX,self.posicionY))
-        
+        enemy = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        fondo = pygame.image.load(font)
+        image = pygame.image.load(self.imagen[Enemy.index_animation])
+        # mix2=pygame.image.load(self.imagen[1])
+        enemy.blit(fondo, (0, 0))
+        enemy.blit(image, (0, 0))
+        self.screen.blit(enemy, (self.posicionX, self.posicionY))
+
         if Enemy.iteration >= 100:
-            Enemy.index_animation=1
+            Enemy.index_animation = 1
         else:
-            Enemy.index_animation=0
-            
-        if Enemy.iteration<=200:
-            Enemy.iteration+=1
+            Enemy.index_animation = 0
+
+        if Enemy.iteration <= 200:
+            Enemy.iteration += 1
         else:
-            Enemy.iteration=0
+            Enemy.iteration = 0
 
-        if str(self.posicionX) +"-"+ str(self.posicionY) not in collision_enemy:
-            collision_enemy.append(str(self.posicionX) +"-"+ str(self.posicionY))
-
-    
-         
-                
-        
-        
-             
+        if str(self.posicionX) + "-" + str(self.posicionY) not in collision_enemy:
+            collision_enemy.append(str(self.posicionX) + "-" + str(self.posicionY))
